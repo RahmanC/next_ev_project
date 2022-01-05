42d5d2854276d9cecc366a357e8c4045bdf52dd6
Abdulrahman Afaraetu <afaraeturahman@gmail.com>
2022-01-05 01:42:01 +0100

    error page

---
+++ pages/404.jsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import { FaExclamationCircle } from 'react-icons/fa'
+import Layout from '../components/Layout'
+import styles from '../styles/404.module.css'
+
+export default function ErrorPage() {
+  return (
+    <Layout title='Page Not Found'>
+      <div className={styles.error}>
+        <h1>
+          <FaExclamationCircle style={{ color: 'red' }} /> 404
+        </h1>
+        <h4>Sorry, Page not found</h4>
+        <Link href='/'>Go Back</Link>
+      </div>
+    </Layout>
+  )
+}
