d7840c688f922e6d712520ed2773980d8bde24a5
Abdulrahman Afaraetu <afaraeturahman@gmail.com>
2022-01-09 13:08:06 +0100

    connecting to strap api

--- pages/events/[slug].jsx
+++ pages/events/[slug].jsx
@@ -1,16 +1,63 @@
+import { FaPencilAlt, FaTimes } from 'react-icons/fa'
 import Layout from '@/components/Layout'
 import { API_URL } from '@/config/index'
+import styles from '@/styles/Event.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function EventPage({ evt }) {
+  const deleteEvent = () => {
+    console.log('delete')
+  }
   return (
     <Layout>
-      <h1>{evt.name}</h1>
+      <div className={styles.event}>
+        <div className={styles.control}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt /> Edit Event
+            </a>
+          </Link>
+          <a href='#' className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </a>
+        </div>
+
+        <span>
+          {evt.date.substring(0, 10)} at {evt.time}
+        </span>
+
+        <h1>{evt.name}</h1>
+        {evt.image && (
+          <div className={styles.image}>
+            <Image
+              src={evt.image.formats.medium.url}
+              alt={evt.name}
+              width={960}
+              height={600}
+            />
+          </div>
+        )}
+
+        <h3>Performers:</h3>
+        <p>{evt.performers}</p>
+
+        <h3>Description</h3>
+        <p>{evt.description}</p>
+
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href='/events'>
+          <a className={styles.back}>{'<'} Go Back</a>
+        </Link>
+      </div>
     </Layout>
   )
 }
 
 export async function getStaticPaths() {
-  const res = await fetch(`${API_URL}/api/events`)
+  const res = await fetch(`${API_URL}/events`)
   const events = await res.json()
 
   const paths = events.map((evt) => ({
@@ -26,7 +73,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const res = await fetch(`${API_URL}/api/events/${slug}`)
+  const res = await fetch(`${API_URL}/events?slug=${slug}`)
   const events = await res.json()
 
   return {
